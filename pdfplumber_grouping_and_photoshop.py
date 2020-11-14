3e24a8b6d94a1b2513e319c16f9c2e1e38885fd7
lan-party <landon.burress@gmail.com>
2020-11-13 20:12:38 -0600

    Update pdfplumber_grouping_and_photoshop.py

--- pdfplumber_grouping_and_photoshop.py
+++ pdfplumber_grouping_and_photoshop.py
@@ -47,10 +47,10 @@ psApp = win32com.client.Dispatch("Photoshop.Application")
 batchcount = 0
 for batch in filltext:
     # Open template file
-    psApp.Open(r"C:\Users\dell\Documents\thelasersedge\automation\template.psd")
+    psApp.Open(r"C:\path\to\template.psd")
     doc = psApp.Application.ActiveDocument
     # Set text for all layers
     for a in range(1, len(batch)):
         doc.ArtLayers[str(a)].TextItem.contents = batch[a-1]
     # Save as a new psd
-    doc.SaveAs(r"C:\Users\dell\Documents\thelasersedge\automation\batch"+str(batchcount)+".psd")
+    doc.SaveAs(r"C:\path\to\batch"+str(batchcount)+".psd")
